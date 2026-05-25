e74efcdfe6573c0191b40415ced456cce5b94742
Popescu Victor <romaniahasmetal@gmail.com>
2026-05-25 10:56:00 +0300

    Opening filedialog (browing files) to get json credentials)

--- firebase_db.py
+++ firebase_db.py
@@ -1,10 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 from functools import lru_cache
+from tkinter import filedialog
+
+def get_credentials():
+    file_path = filedialog.askopenfilename(title="Select Firebase Service Account Key", filetypes=[("JSON files", "*.json")])
+    if not file_path:
+        raise Exception("No file selected for Firebase credentials.")
+    return credentials.Certificate(file_path)
+
 
 @lru_cache(maxsize=1)
 def get_db():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")
+        cred = get_credentials()
         firebase_admin.initialize_app(cred)
     return firestore.client()
+
+get_db()
